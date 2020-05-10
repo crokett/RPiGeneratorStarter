8ca3734d552670ca5286b5a1224d23654324d155
David Green <greendm@us.ibm.com>
2020-05-09 20:52:47 -0400

    gitignore update

--- README.md
+++
@@ -1,2 +0,0 @@
-# RPiGeneratorStarter
-A project to use a Rapsberry Pi and Blynk to remotely start a generator
--- src/testmail.js
+++
@@ -1,9 +0,0 @@
-//#!/usr/bin/env node
-
-var mail = require('/usr/opt/generator/email');
-
-const SEND = "crokett@gmail.com";
-const RECEIVE = "allankgreen@gmail.com";
-
-mail.setOptions(SEND,RECEIVE, "from Node.js on a Pi", "hello from a Pi! My favorite flavor is raspberry");
-mail.sendMail();
